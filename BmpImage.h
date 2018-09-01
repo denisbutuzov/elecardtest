180e5bbb3cad7a7ac296ae3cf0dfbd6b78d7666c
Denis Butuzov <qwaswedo@gmail.com>
2018-09-01 14:38:36 +0700

    Added code in BmpImage header file

--- BmpImage.h
+++ BmpImage.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <string>
+#include <fstream>
+
+#include "bitTypes.h"
+
+struct BITMAPFILEHEADER
+{
+     WORD signature;
+     DWORD fileSize;
+     WORD reserved1;
+     WORD reserved2;
+     DWORD pixelOffset;
+};
+
+struct BITMAPINFOHEADER
+{
+
+};
+
+class BmpImage
+{
+public:
+    BmpImage(std::string name) {}
+private:
+    std::ifstream file_;
+    BITMAPFILEHEADER bmp_;
+    BITMAPINFOHEADER info_;
+};
