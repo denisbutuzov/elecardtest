e65c23901d3739e7293dc480d2758c49ed4555ba
Denis Butuzov <qwaswedo@gmail.com>
2018-09-01 14:52:30 +0700

    Added definitions of BmpImage methods

--- BmpImage.cpp
+++ BmpImage.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+
 #include "BmpImage.h"
+
+BmpImage::BmpImage(std::string name)
+{
+    file_.open(name, std::ios::binary);
+    if(!file_)
+    {
+        std::cerr << "Can not open file " << name << std::endl;
+        exit(1);
+    }
+
+    file_.read(reinterpret_cast<char *>(&bmp_), sizeof(BITMAPFILEHEADER));
+    if(!file_)
+    {
+        std::cerr << "Can not read file " << name << std::endl;
+        exit(1);
+    }
+}
+
+DWORD BmpImage::fileSize() const
+{
+    return bmp_.fileSize;
+}
