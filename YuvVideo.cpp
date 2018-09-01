53de06ea998d6520729ca43d3deaad3e1fcad023
Denis Butuzov <qwaswedo@gmail.com>
2018-09-01 22:39:29 +0700

    Added constructor in the class YuvVideo

--- YuvVideo.cpp
+++ YuvVideo.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <fstream>
+
 #include "YuvVideo.h"
+
+YuvVideo::YuvVideo(const std::string &name)
+{
+    std::ifstream stream(name, std::ios::binary);
+    if(!stream)
+    {
+        std::cerr << "ERROR: Could not open file " << name << "for reading." << std::endl;
+        exit(1);
+    }
+
+}
--- YuvVideo.h
+++ YuvVideo.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <string>
+
+class YuvVideo
+{
+public:
+    YuvVideo(const std::string &name);
+private:
+};
