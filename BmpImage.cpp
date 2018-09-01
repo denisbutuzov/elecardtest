96b7aa5b0983ef86f11e2e7bde3f4e3cc34772d2
Denis Butuzov <qwaswedo@gmail.com>
2018-09-01 19:41:59 +0700

    Added new checks

--- BmpImage.cpp
+++ BmpImage.cpp
@@ -7,7 +7,7 @@ BmpImage::BmpImage(std::string name)
     file_.open(name, std::ios::binary);
     if(!file_)
     {
-        std::cerr << "ERROR: Can not open file " << name << std::endl;
+        std::cerr << "ERROR: Can not open file " << name << "." << std::endl;
         exit(1);
     }
 
@@ -15,7 +15,18 @@ BmpImage::BmpImage(std::string name)
     file_.read(reinterpret_cast<char *>(&info_), sizeof(BITMAPINFOHEADER));
     if(!file_)
     {
-        std::cerr << "ERROR: Can not read file " << name << std::endl;
+        std::cerr << "ERROR: Can not read file " << name << "." << std::endl;
+        exit(1);
+    }
+
+    if(bmp_.type != 19778)
+    {
+        std::cerr << "ERROR: Invalid type value of file " << bmp_.type << ". Expected 19778." << std::endl;
+    }
+
+    if(info_.bitsPerPixel != 24)
+    {
+        std::cerr << "ERROR: Invalid bit deph " << info_.bitsPerPixel << ". Expected 24." << std::endl;
         exit(1);
     }
 
