98b13ca472ff04f830ca86c17f46bb0d79cadb68
Denis Butuzov <qwaswedo@gmail.com>
2018-09-03 15:43:34 +0700

    Replaced definitions of Bitmap headers structure

--- BmpImage.h
+++ BmpImage.h
@@ -7,32 +7,6 @@
 #include "bitTypes.h"
 #include "Yuv420Image.h"
 
-#pragma pack(push, 1)
-struct BITMAPFILEHEADER
-{
-     WORD type;
-     DWORD fileSize;
-     WORD reserved1;
-     WORD reserved2;
-     DWORD pixelOffset;
-};
-#pragma pack(pop)
-
-struct BITMAPINFOHEADER
-{
-    DWORD size;
-    DWORD width;
-    DWORD height;
-    WORD planes;
-    WORD bitsPerPixel;
-    DWORD compression;
-    DWORD imageSize;
-    DWORD x_pixelsPerMeter;
-    DWORD y_pixelsPerMeter;
-    DWORD clr_used;
-    DWORD clr_important;
-};
-
 class BmpImage
 {
 public:
--- bitTypes.h
+++ bitTypes.h
@@ -3,3 +3,29 @@
 typedef unsigned char BYTE;
 typedef unsigned short int WORD;
 typedef unsigned int DWORD;
+
+#pragma pack(push, 1)
+struct BITMAPFILEHEADER
+{
+     WORD type;
+     DWORD fileSize;
+     WORD reserved1;
+     WORD reserved2;
+     DWORD pixelOffset;
+};
+#pragma pack(pop)
+
+struct BITMAPINFOHEADER
+{
+    DWORD size;
+    DWORD width;
+    DWORD height;
+    WORD planes;
+    WORD bitsPerPixel;
+    DWORD compression;
+    DWORD imageSize;
+    DWORD x_pixelsPerMeter;
+    DWORD y_pixelsPerMeter;
+    DWORD clr_used;
+    DWORD clr_important;
+};
