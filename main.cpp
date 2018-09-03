cad4f838df9d6fa8e6b2ac5c2e8cf684078d662f
Denis Butuzov <qwaswedo@gmail.com>
2018-09-03 12:58:39 +0700

    Added code for getting resolution of video file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include <regex>
 
 #include "BmpImage.h"
 #include "Yuv420Image.h"
 
-void check_arg(int argNumber)
+std::pair<int, int> check_arg(int argNumber, char **arg)
 {
     if(argNumber == 1)
     {
@@ -16,11 +17,32 @@ void check_arg(int argNumber)
         std::cerr << "ERROR: More arguments is required." << std::endl;
         exit(1);
     }
+    else if(argNumber == 3)
+    {
+        std::cerr << "ERROR: Video file resolution is required." << std::endl;
+        exit(1);
+    }
+
+    std::regex regExp("(.*)x(.*)");
+    std::smatch match;
+    std::string str = arg[3];
+    if(std::regex_search(str, match, regExp))
+    {
+        std::pair<int, int> resolution;
+        resolution.first = std::stoi(match[1]);
+        resolution.second = std::stoi(match[2]);
+        return resolution;
+    }
+    else
+    {
+        std::cerr << "ERROR: Invalid video file resolution. Expected <width>x<height>. Example: 640x480." << std::endl;
+        exit(1);
+    }
 }
 
 int main(int argc, char **argv)
 {
-    check_arg(argc);
+    std::pair<int, int> resolution = check_arg(argc, argv);
 
     //argv[1] - image.bmp
     BmpImage image(argv[1]);
