e1bde9e9b27e89c93267650c2c52055625173626
Denis Butuzov <qwaswedo@gmail.com>
2018-09-03 15:45:46 +0700

    Fixed bitTypes.h

--- bitTypes.h
+++ bitTypes.h
@@ -1,31 +1,31 @@
 #pragma once
 
-typedef unsigned char BYTE;
-typedef unsigned short int WORD;
-typedef unsigned int DWORD;
+typedef unsigned char       BYTE;
+typedef unsigned short int  WORD;
+typedef unsigned int        DWORD;
 
 #pragma pack(push, 1)
 struct BITMAPFILEHEADER
 {
-     WORD type;
-     DWORD fileSize;
-     WORD reserved1;
-     WORD reserved2;
-     DWORD pixelOffset;
+     WORD   type;
+     DWORD  fileSize;
+     WORD   reserved1;
+     WORD   reserved2;
+     DWORD  pixelOffset;
 };
 #pragma pack(pop)
 
 struct BITMAPINFOHEADER
 {
-    DWORD size;
-    DWORD width;
-    DWORD height;
-    WORD planes;
-    WORD bitsPerPixel;
-    DWORD compression;
-    DWORD imageSize;
-    DWORD x_pixelsPerMeter;
-    DWORD y_pixelsPerMeter;
-    DWORD clr_used;
-    DWORD clr_important;
+    DWORD   size;
+    DWORD   width;
+    DWORD   height;
+    WORD    planes;
+    WORD    bitsPerPixel;
+    DWORD   compression;
+    DWORD   imageSize;
+    DWORD   x_pixelsPerMeter;
+    DWORD   y_pixelsPerMeter;
+    DWORD   clr_used;
+    DWORD   clr_important;
 };
